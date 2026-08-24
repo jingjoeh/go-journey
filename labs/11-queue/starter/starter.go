f2bf4f51ab781f3f7774b6100d6b4207c7e3470b
Komkrit Banglad <kbbl@Komkrits-MacBook-Pro.local>
2026-08-24 20:10:47 +0700

    lab 10.

--- labs/11-queue/starter/starter.go
+++ labs/11-queue/starter/starter.go
@@ -8,5 +8,21 @@ import (
 var ErrInvalidAttempts = errors.New("max attempts must be positive")
 
 func Process(ctx context.Context, maxAttempts int, handle func(context.Context) error) error {
-	panic("TODO: implement Process")
+	if maxAttempts < 1 {
+		return ErrInvalidAttempts
+	}
+	var atErr error
+	for range maxAttempts {
+		if err := ctx.Err(); err != nil {
+			return err
+		}
+		err := handle(ctx)
+		if err == nil {
+			return nil
+		} else {
+			atErr = err
+		}
+	}
+
+	return atErr
 }
